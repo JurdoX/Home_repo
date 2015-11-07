fa9adbe71862b9920a3cbddda7680361e3753536
JurdoX <juscak.juraj@gmail.com>
2015-11-07 20:58:45 +0100

    Lecture 60: loading bean containers with contextLoaderListener

--- springWeb/src/com/jurdox/spring/web/dao/OffersDao.java
+++ springWeb/src/com/jurdox/spring/web/dao/OffersDao.java
@@ -21,24 +21,28 @@ public class OffersDao {
 
 	private NamedParameterJdbcTemplate jdbc;
 
-	@Autowired
+	public OffersDao() {
+		System.out.println("Successfully loaded offers DAO");
+	}
+
+	// @Autowired
 	public void setJdbc(DataSource jdbc) {
 		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
 	}
-	
+
 	public boolean updateOffer(Offer offer) {
 		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
 		return jdbc.update("update offers set name=:name, email=:email, text=:text where id=:id", params) == 1;
 	}
-	
+
 	public boolean createOffer(Offer offer) {
 		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
 		return jdbc.update("insert into offers (name, email, text) values(:name, :email, :text)", params) == 1;
 	}
-	
+
 	@Transactional
 	public int[] createOfferList(List<Offer> offer) {
-		SqlParameterSource [] params = SqlParameterSourceUtils.createBatch(offer.toArray());
+		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offer.toArray());
 		return jdbc.batchUpdate("insert into offers (id, name, email, text) values(:id, :name, :email, :text)", params);
 	}
 
@@ -46,7 +50,7 @@ public class OffersDao {
 		MapSqlParameterSource params = new MapSqlParameterSource("id", id);
 		return jdbc.update("delete from offers where id = :id", params) == 1;
 	}
-	
+
 	public List<Offer> getOffers() {
 		return jdbc.query("select * from offers", new RowMapper<Offer>() {
 
