645a7f48d0641f7249e7f1aff154365ba2ba34bf
JurdoX <juscak.juraj@gmail.com>
2015-11-19 19:14:28 +0100

    all

--- springWeb/src/com/jurdox/spring/web/controllers/OffersController.java
+++ springWeb/src/com/jurdox/spring/web/controllers/OffersController.java
@@ -1,29 +1,45 @@
 package com.jurdox.spring.web.controllers;
 
-import java.util.Map;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jurdox.spring.web.dao.Offer;
+import com.jurdox.spring.web.services.OffersService;
 
 @Controller
 public class OffersController {
 
-	@RequestMapping("/")
-	public ModelAndView showHome() {
-		ModelAndView mv = new ModelAndView("home");
-		Map<String, Object> model = mv.getModel();
-		model.put("name", "<b>Juro</b>");
-		
-		return mv;
+	private OffersService offersService;
+
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
 	}
+
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String showOffers(Model model, @RequestParam("id") String id) {
+		System.out.println("Id is: " + id);
+		return "offers";
+	}
+
 	
-//	@RequestMapping("/")
-//	public String showHome(Model model) {
-//		model.addAttribute("name", "Cecil");
-//		
-//		return "home";
-//	}
-	
+	@RequestMapping("/offers")
+	public String showOffers(Model model) {
+		List<Offer> offers = offersService.getCurrent();
+		model.addAttribute("offers", offers);
+
+		return "offers";
+	}
+
+	@RequestMapping("/createoffer")
+	public String createOffer() {
+		return "createoffer";
+	}
+
 }
---
+++ springWeb/src/com/jurdox/spring/web/services/OffersService.java
@@ -0,0 +1,24 @@
+package com.jurdox.spring.web.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jurdox.spring.web.dao.Offer;
+import com.jurdox.spring.web.dao.OffersDao;
+
+@Service("offersService")
+public class OffersService {
+
+	private OffersDao offersDao;
+
+	@Autowired
+	public void setOffersDao(OffersDao offersDao) {
+		this.offersDao = offersDao;
+	}
+
+	public List<Offer> getCurrent() {
+		return offersDao.getOffers();
+	}
+}
