ed0060b07dc6461ecc8d7a8ed0b8ecd2e0e3c69c
JurdoX <juscak.juraj@gmail.com>
2016-03-05 23:17:43 +0100

    added log4j and log4j.properties file

--- springWeb/src/com/jurdox/spring/web/controllers/HomeController.java
+++ springWeb/src/com/jurdox/spring/web/controllers/HomeController.java
@@ -1,13 +1,17 @@
 package com.jurdox.spring.web.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+	private static Logger log = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
 	public String showHome() {
+		log.info("showing homepage...");
 		return "home";
 	}
 }
